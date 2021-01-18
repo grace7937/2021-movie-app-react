d1b4e96ed44916047d83e96adc652e9969ead45e
grace7937 <66059987+grace7937@users.noreply.github.com>
2021-01-18 21:10:04 +0900

    20210118

--- src/App.js
+++ src/App.js
@@ -1,49 +1,18 @@
-import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import './App.css';
-
+import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Detail from './routes/Detail';
+import Navigation from './components/Navigation';
 
 function App() {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-    getMovies = async () => {
-      const {
-        data : {
-          data : {movies}
-        }
-      } = await axios.get(
-        'https://yts-proxy.nomadcoders1.now.sh/list_movies.json'
-      );
-      this.setState({movies, isLoading: false});
-    };
-
-    componentDidMount() {
-      this.getMovies();
-    }
-    render () {
-      const { isLoading} = this.state;
-      return <div>{isLoading ? 'Loading...' :'We are ready'}</div>;
-    }
-  }
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path='/' exact={true} component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/movie-detail' component={Detail} />
+    </HashRouter>
+  );
+}
 export default App;
-
-
-// {siLoading ? (
-//   <div className='loader'>
-//     <span className='Loader__text'>Loading...</span>
-//   </div>
-// ) : (<div className='movies'>
-//   {movies.map(movie => {
-//     <Movie
-//     key = {movie.id}
-//     id = {movie.id}
-//     year = {movie.year}
-//     title = {movie.title}
-//     summary = {movie.summary}
-//     poster = {movie.mediym_cover_image}
-
-//   })} </div>
-// </section>
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About() {
+  return <sapn>About this page</sapn>;
+}
+
+export default About;
